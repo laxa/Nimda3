eb1bd11da368b18b2d530cb8e65ed1983bd94c7e
laxa <L4x4t0r@gmail.com>
2014-09-23 21:56:36 +0200

    typo plugin_Zmachine; Add Plugin_DDNetMapRelease

---
+++ plugins/user/Plugin_DDNetMapRelease.php
@@ -0,0 +1,45 @@
+<?php
+
+class Plugin_DDNetMapRelease extends Plugin
+{
+	public $triggers = array('!ddnetmaprelease');
+	public $interval = 300;
+	public $hideFromHelp = true;
+	public $enabledByDefault = false;
+	private $last;
+
+	function onLoad()
+	{
+	  $this->last = $this->getVar('ddnetlatestmaprelease');
+	  if(!$this->last)
+	    $this->last = date('Y-m-d H:i');
+	}
+
+	function onInterval()
+	{
+	  $page = libHTTP::GET('http://ddnet.tw/releases/');
+	  if ($page === FALSE || strlen($page) == 0) return;
+	  $array = array();
+	  $regex = '#(\d{4}-\d{2}-\d{2} \d{2}:\d{2}).+href="(\/ranks\/(moderate|novice|solo|brutal|oldschool)\/\#map-[^"]+)"><span title[^>]+>([^<]+)(.+mappers[^>]+>([^<]+))?#';
+	  preg_match_all($regex, $page, $array);
+	  for ($i = 0; $i < count($array[1]); $i++)
+	    {
+	      if (strtotime($this->last) >= strtotime($array[1][$i])) break;
+	      $released = $array[1][$i];
+	      $url = 'http://ddnet.tw'.$array[2][$i];
+	      $difficulty = $array[3][$i];
+	      $map = $array[4][$i];
+	      $mapper = $array[6][$i];
+	      $format = "\x02".$map."\x02 by \x02".$mapper."\x02 just released on ".$difficulty." at ".$released." : ".$url;
+	      $this->sendToEnabledChannels($format);
+	    }
+	  $this->last = $array[1][0];
+	}
+
+	function onUnload()
+	{
+	  $this->saveVar('ddnetlatestmaprelease', $this->last);
+	}
+}
+
+?>
