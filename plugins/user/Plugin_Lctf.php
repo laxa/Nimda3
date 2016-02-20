a31266a7dc73cd6e45cf069bf9c2fd0eb6a4b996
laxa <laxa@ddracepro.net>
2016-02-20 13:37:41 +0100

    Updating interval for Lctf

--- plugins/user/Plugin_Lctf.php
+++ plugins/user/Plugin_Lctf.php
@@ -4,7 +4,7 @@ class Plugin_Lctf extends Plugin
 {
     public $triggers = array('!lctf');
     public $helpText = 'Display our stats or team\'s rank if given';
-    public $interval = 300;
+    public $interval = 1800;
     public $enabledByDefault = false;
     private $json;
     private $teamToWatch = array('0x90r00t', 'khack40', 'pony7', 'LSE');
