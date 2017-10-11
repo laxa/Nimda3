69ec53d9146c31c52a26721d7145f31773caf7c2
laxa <laxa@ddracepro.net>
2017-10-11 21:22:10 +0200

    Add ethereum price plugin

---
+++ plugins/user/Plugin_EthereumPrice.php
@@ -0,0 +1,28 @@
+<?php
+
+class Plugin_EthereumPrice extends Plugin
+{
+	public $triggers = array('!eth');
+	public $interval = 120;
+	public $helpText = 'Prints the current ethereum price.';
+
+	function isTriggered()
+	{
+        $data = file_get_contents('https://api.kraken.com/0/public/Ticker?pair=ETHEUR');
+        if ($data === false)
+            {
+                $this->reply("Error while retrieving ETH data");
+                return;
+            }
+        $data = file_get_contents('https://api.kraken.com/0/public/Ticker?pair=ETHEUR');      
+        $data = json_decode($data, true);
+        if (is_array($data) == false || !isset($data['result']['XETHZEUR']['a'][0]))
+            {
+                $this->reply("Error while parsing data");
+                return;
+            }
+        $this->reply(sprintf("ETH price: %.2f€", $data['result']['XETHZEUR']['a'][0]));
+	}
+}
+
+?>
